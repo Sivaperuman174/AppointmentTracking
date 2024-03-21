e220087f8ba41fa44c0ea1d9c1308fef3b23ce10
Siva_Peruman <sivaperuman174@gmail.com>
2024-03-21 20:04:59 +0000

    appointment create and request detail added

--- lib/view/appointment_create_page.dart
+++ lib/view/appointment_create_page.dart
@@ -1,27 +1,34 @@
 import 'package:appointmenttrackerapp/model/appointment.dart';
+import 'package:appointmenttrackerapp/model/request.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class AppointmentEditPage extends StatefulWidget {
-  final Appointment appointmentDetail;
-  void Function() updateAppointmentHandler;
+typedef AddAppointmentHandler = void Function(Appointment newAppointment);
 
-  AppointmentEditPage(
+class AppointmentCreatePage extends StatefulWidget {
+  final Request requestDetail;
+  final AddAppointmentHandler addAppointmentHandler;
+  final int totalAppointmentCount;
+
+  const AppointmentCreatePage(
       {super.key,
-      required this.appointmentDetail,
-      required this.updateAppointmentHandler});
+      required this.requestDetail,
+      required this.addAppointmentHandler,
+      required this.totalAppointmentCount});
 
   @override
-  State<AppointmentEditPage> createState() => _AppointmentEditPageState();
+  State<AppointmentCreatePage> createState() => _AppointmentCreatePageState();
 }
 
-class _AppointmentEditPageState extends State<AppointmentEditPage> {
-  late TextEditingController _descriptionController;
-  late TextEditingController _allocateToController;
-  late TextEditingController _alternateAllocateToController;
-  late TextEditingController _actualDateController;
-  late TextEditingController _alternateDateController;
-  late TextEditingController _statusController;
+class _AppointmentCreatePageState extends State<AppointmentCreatePage> {
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _allocateToController = TextEditingController();
+  final TextEditingController _alternateAllocateToController =
+      TextEditingController();
+  final TextEditingController _actualDateController = TextEditingController();
+  final TextEditingController _alternateDateController =
+      TextEditingController();
+  final TextEditingController _statusController = TextEditingController();
 
   DateTime? _selectedActualDate;
   DateTime? _selectedAlternateDate;
@@ -29,22 +36,6 @@ class _AppointmentEditPageState extends State<AppointmentEditPage> {
   @override
   void initState() {
     super.initState();
-    _descriptionController =
-        TextEditingController(text: widget.appointmentDetail.description ?? '');
-    _allocateToController = TextEditingController(
-        text: widget.appointmentDetail.allocatedTo.toString() ?? '');
-    _alternateAllocateToController = TextEditingController(
-        text: widget.appointmentDetail.alternateAllocateTo.toString() ?? '');
-    _actualDateController = TextEditingController(
-        text: DateFormat('dd-MM-yyyy')
-                .format(widget.appointmentDetail.actualDate) ??
-            '');
-    _alternateDateController = TextEditingController(
-        text: DateFormat('dd-MM-yyyy')
-                .format(widget.appointmentDetail.alternateDate) ??
-            '');
-    _statusController =
-        TextEditingController(text: widget.appointmentDetail.status ?? '');
   }
 
   @override
@@ -61,15 +52,22 @@ class _AppointmentEditPageState extends State<AppointmentEditPage> {
   // Function to save employee details
   void _saveAppointmentDetails() {
     // Implement saving logic here
-    widget.appointmentDetail.description = _descriptionController.text;
-    widget.appointmentDetail.allocatedTo =
-        int.parse(_allocateToController.text);
-    widget.appointmentDetail.alternateAllocateTo =
-        int.parse(_alternateAllocateToController.text);
-    widget.appointmentDetail.actualDate = _selectedActualDate!;
-    widget.appointmentDetail.alternateDate = _selectedAlternateDate!;
-    widget.appointmentDetail.status = _statusController.text;
-    widget.updateAppointmentHandler();
+    Appointment newAppointment = Appointment(
+        id: widget.totalAppointmentCount + 1,
+        clientId: widget.requestDetail.clientId,
+        requestId: widget.requestDetail.id,
+        createdBy: 1,
+        allocatedTo: int.parse(_allocateToController.text),
+        alternateAllocateTo: int.parse(_alternateAllocateToController.text),
+        description: _descriptionController.text,
+        bookedOn: DateTime.now(),
+        actualDate: _selectedActualDate!,
+        alternateDate: _selectedAlternateDate!,
+        status: "Created",
+        deleted: 0);
+
+    widget.requestDetail.status = "AppointmentCreated";
+    widget.addAppointmentHandler(newAppointment);
     Navigator.pop(context);
   }
 
@@ -86,7 +84,7 @@ class _AppointmentEditPageState extends State<AppointmentEditPage> {
           children: [
             TextField(
               controller: _descriptionController,
-              decoration: const InputDecoration(labelText: 'Name'),
+              decoration: const InputDecoration(labelText: 'Description'),
             ),
             TextField(
               controller: _allocateToController,
@@ -149,11 +147,11 @@ class _AppointmentEditPageState extends State<AppointmentEditPage> {
                 ),
               ),
             ),
-            TextField(
-              controller: _statusController,
-              decoration: const InputDecoration(labelText: 'Status'),
-            ),
-            const SizedBox(height: 20),
+            // TextField(
+            //   controller: _statusController,
+            //   decoration: const InputDecoration(labelText: 'Status'),
+            // ),
+            const SizedBox(height: 30),
             Row(
               children: [
                 ElevatedButton(
--- lib/view/request_detail_page.dart
+++ lib/view/request_detail_page.dart
@@ -1,4 +1,9 @@
+import 'package:appointmenttrackerapp/model/appointment.dart';
+import 'package:appointmenttrackerapp/model/client.dart';
+import 'package:appointmenttrackerapp/model/request.dart';
+import 'package:appointmenttrackerapp/view/appointment_create_page.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RequestDetailPage extends StatefulWidget {
   const RequestDetailPage({Key? key}) : super(key: key);
@@ -8,8 +13,186 @@ class RequestDetailPage extends StatefulWidget {
 }
 
 class _RequestDetailPageState extends State<RequestDetailPage> {
+  Client? _selectedClient;
+  final List<Client> clients = [
+    Client(
+      id: 1,
+      name: "Pressform",
+      companyId: 1,
+      address: "main road",
+      primaryContactName: "suresh",
+      primaryContactNo: "44554433",
+      secondaryContactName: "kumar",
+      secondaryContactNo: '9988777',
+      appointments: [],
+      requests: [
+        Request(
+            id: 1,
+            clientId: 1,
+            requestType: 'New',
+            description: 'New product required',
+            requestedOn: DateTime.now(),
+            status: 'Created',
+            deleted: 0),
+        Request(
+            id: 2,
+            clientId: 1,
+            requestType: 'Issue',
+            description: 'Issue in existing product',
+            requestedOn: DateTime.now(),
+            status: 'Created',
+            deleted: 0),
+      ],
+      location: '12.34,19.87',
+      deleted: 0,
+    ),
+    Client(
+      id: 2,
+      name: "chennai Cnc",
+      companyId: 1,
+      address: "Cross road",
+      primaryContactName: "mani",
+      primaryContactNo: "5533222",
+      secondaryContactName: "bala",
+      secondaryContactNo: '00998877',
+      appointments: [],
+      requests: [
+        Request(
+            id: 1,
+            clientId: 1,
+            requestType: 'Issue',
+            description: 'issue in new product',
+            requestedOn: DateTime.now(),
+            status: 'Created',
+            deleted: 0),
+        Request(
+            id: 2,
+            clientId: 1,
+            requestType: 'Issue',
+            description: 'Issue in existing product',
+            requestedOn: DateTime.now(),
+            status: 'AppointmentCreated',
+            deleted: 0),
+      ],
+      location: '12.34,19.87',
+      deleted: 0,
+    ),
+  ];
+
+  void addAppointment(Appointment newAppointment) {
+    setState(() {
+      _selectedClient!.appointments?.add(newAppointment);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('this is request detail page');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Requests detail'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              const Text(
+                'Select Client :',
+                style: TextStyle(fontSize: 15),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              DropdownButton<Client>(
+                value: _selectedClient,
+                items: clients.map((Client item) {
+                  return DropdownMenuItem<Client>(
+                    value: item,
+                    child: Text(item.name),
+                  );
+                }).toList(),
+                onChanged: (Client? selectedItem) {
+                  setState(() {
+                    _selectedClient = selectedItem;
+                  });
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          _selectedClient != null
+              ? Expanded(
+                  child: ListView.builder(
+                    itemCount: _selectedClient!.requests?.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Card(
+                                child: ListTile(
+                                  title: Text(_selectedClient!
+                                      .requests![index].description),
+                                  subtitle: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                          'Request Id: ${_selectedClient!.requests![index].id}'),
+                                      Text(
+                                          'Request type: ${_selectedClient!.requests![index].requestType}'),
+                                      Text(
+                                          'Description: ${_selectedClient!.requests![index].description}'),
+                                      Text(
+                                          'Status: ${_selectedClient!.requests![index].status}'),
+                                      Text(
+                                          'Requested On: ${DateFormat('dd-MM-yyyy').format(_selectedClient!.requests![index].requestedOn)}'),
+                                      // Text(
+                                      //     'Completed On: ${DateFormat('dd-MM-yyyy').format(_selectedClient!.requests![index].completedOn!)}'),
+                                    ],
+                                  ),
+                                  trailing: _selectedClient!
+                                              .requests![index].status !=
+                                          "AppointmentCreated"
+                                      ? IconButton(
+                                          icon: const Icon(Icons.create),
+                                          tooltip: 'Create Appointment',
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    AppointmentCreatePage(
+                                                        requestDetail:
+                                                            _selectedClient!
+                                                                    .requests![
+                                                                index],
+                                                        addAppointmentHandler:
+                                                            addAppointment,
+                                                        totalAppointmentCount:
+                                                            _selectedClient!
+                                                                    .appointments
+                                                                    ?.length ??
+                                                                0),
+                                              ),
+                                            );
+                                          },
+                                        )
+                                      : Text(''),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                )
+              : const Text('No Request to display'),
+        ],
+      ),
+    );
   }
 }
